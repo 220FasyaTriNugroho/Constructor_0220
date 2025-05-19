51eee8b65bd925ebb34367d9211a2fc5c25691e1
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-05-19 18:23:17 +0700

    membuat objek mahasiswa

--- activity9.cpp
+++ activity9.cpp
@@ -4,6 +4,17 @@ using namespace std;
 class Mahasiswa{
     private:
         static int totalMahasiswa;
+    public:
+        string nama;
+        string status;
+        int umur;
+        
+        Mahasiswa(string pNama, int pUmur) {
+            nama = pNama;
+            umur = pUmur;
+            status = "Mahasiswa Baru";
+            ++totalMahasiswa;
+        }
 };
 
 int main(){
